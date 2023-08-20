e83316cafc895f30ab0560e0920c085f0c185cc6
Kuririn <kuririn1@gmail.com>
2023-08-20 21:28:44 +0200

    refactor + new account/followers trigger

--- index.js
+++ index.js
@@ -5,68 +5,86 @@ import fs from "fs";
 config();
 
 const url = process.env.WEBSOCKET_URL;
+const FOLLOW_NUM = 100;
 const contractAddress = '0xCF205808Ed36593aa40a44F10c7f7C2F67d4A4d4';
-
-const rawdata = fs.readFileSync('contractABI.json', 'utf8');
-const contractABI = JSON.parse(rawdata);
-
+const contractABI = JSON.parse(fs.readFileSync('contractABI.json', 'utf8'));
 const provider = new ethers.WebSocketProvider(url);
-
 const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
-contract.on('Trade', async (trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply, event) => {
-    const boughtSold = (await getUserData(subject)).twitterUsername;
-    console.log({
-        trader: (await getUserData(trader)).twitterUsername,
-        subject: boughtSold,
+contract.on('Trade', async (trader, subject, isBuy, shareAmount, ethAmount, protocolEthAmount, subjectEthAmount, supply) => {
+    const basicTradeDetails = {
+        traderAddress: trader,
+        subjectAddress: subject,
         isBuy: isBuy ? "Buy" : "Sell",
         shareAmount,
         ethAmount: ethers.formatEther(ethAmount),
         protocolEthAmount: ethers.formatEther(protocolEthAmount),
         subjectEthAmount: ethers.formatEther(subjectEthAmount),
-        supply
-    });
+        supply,
+    };
 
+    if (isNewAccount(basicTradeDetails)) {
+        const traderData = await getUserData(trader);
+        const subjectData = await getUserData(subject);
 
-    if(!boughtSold.startsWith('0x')) {
-        const followers = await getTwitterFollowersCount(boughtSold);
-        console.log(`${boughtSold} followers: ${followers}}`);
+        const tradeDetails = {
+            ...basicTradeDetails,
+            trader: traderData.twitterUsername,
+            subject: subjectData.twitterUsername,
+        };
+
+        if (await hasFollowers(tradeDetails, FOLLOW_NUM)) {
+            console.log(tradeDetails);
+        }
     }
-    
 });
 
-async function getUserData(address) {
-    const url = `https://prod-api.kosetto.com/users/${address}`;
+function isNewAccount({ traderAddress, subjectAddress, isBuy, ethAmount, shareAmount, supply }) {
+    return traderAddress === subjectAddress && isBuy === "Buy" && ethAmount === '0.0' && shareAmount === 1n && supply === 1n;
+}
 
-    const response = await fetch(url);
-
-    if (!response.ok) {
-        return { twitterUsername: address }; // sometimes it fails to fetch, just just address then
-        //throw new Error(`Failed to fetch user data for address ${address}: ${response.statusText}`);
+async function hasFollowers({ subject }, followerNumber) {
+    if (!subject.startsWith('0x')) {
+        const followers = await getTwitterFollowersCount(subject);
+        if (followers > followerNumber) {
+            console.log(`${subject} has ${followers} followers`);
+            return true;
+        }
     }
+    return false;
+}
 
-    const data = await response.json();
-    return data;
+async function getUserData(address) {
+    try {
+        const response = await fetch(`https://prod-api.kosetto.com/users/${address}`);
+        if (response.ok) {
+            return await response.json();
+        }
+    } catch (err) {
+        console.error(`Failed to fetch user data for address ${address}: ${err.message}`);
+    }
+    return { twitterUsername: address };
 }
 
 async function getTwitterFollowersCount(profileName) {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${process.env.TWITTER_TOKEN}`);
+    const myHeaders = new Headers({
+        "Authorization": `Bearer ${process.env.TWITTER_TOKEN}`
+    });
 
-    var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
     };
 
-    const response = await fetch(`https://api.twitter.com/1.1/users/lookup.json?screen_name=${profileName}`, requestOptions);
-
-    if(!response.ok) {
-        console.log(response);
-        throw new Error(`Failed to fetch follow count for ${profileName}: ${response.statusText}`);
+    try {
+        const response = await fetch(`https://api.twitter.com/1.1/users/lookup.json?screen_name=${profileName}`, requestOptions);
+        if (response.ok) {
+            const data = await response.json();
+            return data[0].followers_count;
+        }
+    } catch (err) {
+        console.error(`Failed to fetch follow count for ${profileName}: ${err.message}`);
     }
-
-    const data = await response.json();
-    return data[0].followers_count;
+    return 0;
 }
-
